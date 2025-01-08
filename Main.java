650f55cae988fa9c2db84a118507b6caea102781
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2025-01-08 11:49:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,26 @@
 public class Main {
-    boolean[][] schedule = new boolean[8][60];
-    AppointmentBook a = new AppointmentBook(schedule);
-    for(int i = 10; i < 15; i++){
-        schedule[1][i] = true;
+    public static void main(String[] args) {
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for (int i = 10; i < 15; i++) {
+            schedule[1][i] = true;
+        }
+        for (int i = 10; i < 15; i++) {
+            schedule[1][i] = true;
+        }
+        for (int i = 10; i < 15; i++) {
+            schedule[1][i] = true;
+        }
+        a.printPeriod(2);
+        AppointmentBook b = new AppointmentBook(schedule);
+        int period = 2;
+        while(period < 5){
+            System.out.println("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
+        b.printPeriod(4);
+        System.out.println(b.makeAppointment(2, 4, 22));
+        b.printPeriod(4);
     }
-    for(int i = 10; i < 15; i++){
-        schedule[1][i] = true;
-    }
-    for(int i = 10; i < 15; i++){
-        schedule[1][i] = true;
-    }
-    a.printPeriod(2);
 }
