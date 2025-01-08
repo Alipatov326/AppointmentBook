92522d2aca87701d53aa1666796ae50f3a146985
Alipatov326 <155419066+Alipatov326@users.noreply.github.com>
2025-01-08 11:49:36 -0500

    Update AppointmentBook.java

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -3,7 +3,7 @@ public class AppointmentBook {
         for(int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
     }
-        private boolean[][] schedule;
+    private boolean[][] schedule;
     public AppointmentBook(boolean[][] schedule){
         this.schedule = schedule;
     }
@@ -24,7 +24,10 @@ public class AppointmentBook {
      * 1 <= duration <= 60
      */
     private void reserveBlock(int period, int startMinute, int duration) {
-        /* implementation not shown */ }
+        for(int i = startMinute; i < startMinute + duration; i++){
+            schedule[period - 1][i] = false;
+        }
+    }
 
     /**
      * Searches for the first block of duration free minutes during period, as
@@ -35,7 +38,17 @@ public class AppointmentBook {
      * Preconditions: 1 <= period <= 8; 1 <= duration <= 60
      */
     public int findFreeBlock(int period, int duration) {
-        return 0;
+        int block = 0;
+        for(int i = 0; i < 60; i++){
+            if(isMinuteFree(period, i)){
+                block++;
+            if(block == duration){
+                return i - duration + 1;
+            }
+            else block = 0;
+            }
+        }
+        return -1;
         /* to be implemented in part (a) */ }
 
     /**
@@ -47,8 +60,22 @@ public class AppointmentBook {
      * Preconditions: 1 <= startPeriod <= endPeriod <= 8; 1 <= duration <= 60
      */
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+        schedule = new boolean[8][60];
+        for(int i = 25; i < 30; i++) schedule[1][i] = true;
+        for(int i = 0; i < 15; i++) schedule[2][i] = true;
+        for(int i = 41; i < 60; i++) schedule[2][i] = true;
+        for(int i = 5; i < 30; i++) schedule[3][i] = true;
+        for(int i = 44; i < 60; i++) schedule[3][i] = true;
+        AppointmentBook a = new AppointmentBook(schedule);
+        for(int i = startPeriod; i<= endPeriod; i++){
+            int freeBlock = findFreeBlock(i, duration);
+            if (freeBlock > -1){
+                reserveBlock(i, freeBlock, duration);
+                return true;
+            }
+        }
         return false;
-        /* to be implemented in part (b) */ }
+    }
 
     // There may be instance variables, constructors, and methods that are not shown.
 
